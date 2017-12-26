48a93accb984319a764cc24691166cadf7fe48c2
Tomasz <radwan.tomasz@gmail.com>
2017-12-26 18:50:26 +0000

    Task 4 - brackets validation

---
+++ app/models/concerns/title_brackets_validator.rb
@@ -0,0 +1,26 @@
+require 'set'
+
+class TitleBracketsValidator < ActiveModel::Validator
+    include ActiveModel::Validations
+
+    def brackets_validate(title)
+      stack  = []
+      lookup = { '(' => ')', '[' => ']', '{' => '}', '<' => '>' }
+      left   = lookup.keys
+      right  = lookup.values
+
+      title.each_char do |char|
+        if left.include? char
+          stack << char
+        elsif right.include? char
+          return false if stack.empty? || (lookup[stack.pop] != char)
+        end
+      end
+  
+      if (stack.empty? && !(title.include?('[]') || title.include?('{}') || title.include?('()')))
+          return true
+      else
+          return false
+      end 
+    end
+end
